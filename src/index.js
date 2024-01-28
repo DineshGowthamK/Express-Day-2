879a7d27977e3a6367a3541edb6c26c569b87637
DineshGowthamK <dineshgowtham33@gmail.com>
2024-01-29 00:54:12 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 import express from "express"
 import AppRoutes from './routes/index.js'
 import { MongoClient } from "mongodb"
+import cors from 'cors'
 import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT
+
+app.use(cors())
 app.use(express.json())
 app.use('/',AppRoutes)
 
